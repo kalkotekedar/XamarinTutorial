5695f037ffb323ab73fe2226fd635ddb5712a4aa
K K <kalkotekedar@gmail.com>
2017-08-08 12:14:15 +0530

    Added the code for SQLite asynchronos

--- DataStorageDemo/DataStorageDemo/Models/PersonRepository.cs
+++ DataStorageDemo/DataStorageDemo/Models/PersonRepository.cs
@@ -2,15 +2,20 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace DataStorageDemo.Models
 {
     public sealed class PersonRepository
     {
-        private SQLiteConnection conn;
-        
         // Singleton of the database repository object.
         private static PersonRepository instance;
+
+        private SQLiteConnection conn;
+
+        //Sqlite connection for async methods
+        private SQLiteAsyncConnection conn1;
+
         public static PersonRepository Instance
         {
             get
@@ -22,13 +27,34 @@ namespace DataStorageDemo.Models
             }
         }
 
+        //public static void Initialize(string filename)
+        //{
+        //    if (filename == null)
+        //        throw new ArgumentNullException(nameof(filename));
+
+        //    if (instance != null)
+        //        instance.conn.Dispose();
+
+        //    instance = new PersonRepository(filename);
+        //}
+
+        //private PersonRepository(string dbPath)
+        //{
+        //    //Create SQLite Connection
+        //    conn = new SQLiteConnection(dbPath);
+
+        //    //Create a SQLite Table
+        //    conn.CreateTable<Person>();
+        //}
+
+        /** This is the constructor for async method **/
         public static void Initialize(string filename)
         {
             if (filename == null)
                 throw new ArgumentNullException(nameof(filename));
 
             if (instance != null)
-                instance.conn.Dispose();
+                instance.conn1.GetConnection().Dispose();
 
             instance = new PersonRepository(filename);
         }
@@ -36,14 +62,14 @@ namespace DataStorageDemo.Models
         private PersonRepository(string dbPath)
         {
             //Create SQLite Connection
-            conn = new SQLiteConnection(dbPath);
+            conn1 = new SQLiteAsyncConnection(dbPath);
 
             //Create a SQLite Table
-            conn.CreateTable<Person>();
+            conn1.CreateTableAsync<Person>().Wait();
         }
 
         public string StatusMessage { get; set; }
-       
+
         public void AddNewPerson(string name)
         {
             int result = 0;
@@ -73,6 +99,34 @@ namespace DataStorageDemo.Models
             catch (Exception ex)
             {
                 StatusMessage = string.Format("Failed to retrieve data. {0}", ex.Message);
+            }
+
+            return Enumerable.Empty<Person>();
+        }
+
+        /** Async methods for database operations **/
+        public async Task AddPersonAsync(string name) {
+            int result = 0;
+            try {
+                if (string.IsNullOrEmpty(name))
+                    throw new Exception("String cann't be empty or invalid");
+
+                result = await conn1.InsertAsync(new Person { Name = name });
+
+                StatusMessage = string.Format("{0} records added successful [Name : {1}]", result, name);
+            }
+            catch (Exception e) {
+                StatusMessage = string.Format("Failed to add {0} . Error {1}", name, e.Message);
+            }
+        }
+
+        public async Task<IEnumerable<Person>> GetAllPersonAsync() {
+            try
+            {
+                return await conn1.Table<Person>().ToListAsync();
+            }
+            catch (Exception ex) {
+                StatusMessage = string.Format("Failed to retrive data. {0}", ex.Message);
             }
 
             return Enumerable.Empty<Person>();
--- DataStorageDemo/DataStorageDemo/SQLitePage.xaml.cs
+++ DataStorageDemo/DataStorageDemo/SQLitePage.xaml.cs
@@ -10,27 +10,38 @@ using Xamarin.Forms.Xaml;
 
 namespace DataStorageDemo
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class SQLitePage : ContentPage
-	{
-		public SQLitePage ()
-		{
-			InitializeComponent ();            
-		}
-
-        public void OnNewButtonClicked(object sender, EventArgs args)
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SQLitePage : ContentPage
+    {
+        public SQLitePage()
         {
-            statusMessage.Text = string.Empty;
-            PersonRepository.Instance.AddNewPerson(NewPerson.Text);
-            NewPerson.Text = "";
-            statusMessage.Text = PersonRepository.Instance.StatusMessage;
+            InitializeComponent();
         }
 
-        public void OnGetButtonClicked(object sender, EventArgs args)
+        public async void OnNewButtonClickedAsync(object sender, EventArgs args)
         {
+            //statusMessage.Text = string.Empty;
+            //PersonRepository.Instance.AddNewPerson(NewPerson.Text);
+            //NewPerson.Text = "";
+            //statusMessage.Text = PersonRepository.Instance.StatusMessage;
+
+            /** Async call**/
             statusMessage.Text = string.Empty;
-            var people = PersonRepository.Instance.GetAllPeople();
-            PersonList.ItemsSource = people;
+            await PersonRepository.Instance.AddPersonAsync(NewPerson.Text);
+            statusMessage.Text = PersonRepository.Instance.StatusMessage;
+            NewPerson.Text = "";
+        }
+
+        public async void OnGetButtonClickedAsync(object sender, EventArgs args)
+        {
+            //statusMessage.Text = string.Empty;
+            //var people = PersonRepository.Instance.GetAllPeople();
+            //PersonList.ItemsSource = people;
+
+            /** Async call**/
+            statusMessage.Text = string.Empty;
+            var persons = await PersonRepository.Instance.GetAllPersonAsync();
+            PersonList.ItemsSource = persons;
         }
     }
 }
